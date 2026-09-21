06160159d99d6a08394d02f32add902607118a3b
birdnerd22 <juliamasters10@gmail.com>
2026-09-21 18:51:19 -0400

    Update MechanumDrive.java
    again updated hehe will push tomorrow hopefully

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MechanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MechanumDrive.java
@@ -15,10 +15,10 @@ public class MechanumDrive{
     private IMU imu;
 
     public void init(HardwareMap hwMap){
-        frontLeftMotor = hwMap.get(DcMotor.class, "front_left_motor");
-        backLeftMotor = hwMap.get(DcMotor.class, "back_left_motor");
-        frontRightMotor = hwMap.get(DcMotor.class, "front_right_motor");
-        backRightMotor = hwMap.get(DcMotor.class, "back_right_motor");
+        frontLeftMotor = hwMap.get(DcMotor.class, "FLmotor");
+        backLeftMotor = hwMap.get(DcMotor.class, "BLmotor");
+        frontRightMotor = hwMap.get(DcMotor.class, "FRmotor");
+        backRightMotor = hwMap.get(DcMotor.class, "BRmotor");
 
         frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
