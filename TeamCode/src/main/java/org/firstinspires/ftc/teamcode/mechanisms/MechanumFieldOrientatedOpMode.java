e3c94cf327ba4f106acdad415f408432f7ce2ca6
birdnerd22 <juliamasters10@gmail.com>
2026-09-21 16:03:54 -0400

    Update MechanumFieldOrientatedOpMode.java
    hopefully finished drivebase code I will be so hyped to use this and push code??

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MechanumFieldOrientatedOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/MechanumFieldOrientatedOpMode.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
-public class MechanumFieldOrientatedOpMode extends OpMode{
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class MechanumFieldOrientatedOpMode extends OpMode {
+    MechanumDrive drive = new MechanumDrive();
+    double forward, strafe, rotate;
+
+    @Override
+    public void init(){
+        drive.init(hardwareMap);
+
+    }
+    @Override
+    public void loop(){
+        forward = gamepad1.left_stick_y;
+        strafe = gamepad1.left_stick_x;
+        rotate = gamepad1.right_stick_x;
+
+        drive.driveFieldRelative(forward, strafe, rotate);
+
+    }
 }
